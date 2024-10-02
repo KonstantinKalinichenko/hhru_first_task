ed5db2fa7cde9eeafd1c67225341a73da7155255
Konstantin Kalinichenko <152182945+KonstantinKalinichenko@users.noreply.github.com>
2024-10-02 20:10:50 +0300

    fixed the bug when there is two or more students with the same name

--- Practice.py
+++ Practice.py
@@ -1,10 +1,13 @@
 def get_passed():
     scores = input("Please enter the  students' scores separated by commas:")
     students = input("Please enter the students' names separated by commas:")
+
     stud_list = students.split(',')
     score_list = list(map(int, scores.split(',')))
-    stud_score = dict(zip(stud_list, score_list))
-    passed = ', '.join([k for k, v in stud_score.items() if v >= 35])
+
+    stud_score = [(stud.strip(), score) for stud, score in zip(stud_list, score_list)]
+    passed = ', '.join([name for name, score in stud_score if score >= 35])
+
     if passed:
         return f"{passed} passed the exam!"
     else:
